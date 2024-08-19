e90a64401c701781225d8120ee30b99c33b89855
Pavel <115949945+botanjke@users.noreply.github.com>
2024-08-19 22:19:27 +0300

    Update seabattle.js

--- teachbook/seabattle.js
+++ teachbook/seabattle.js
@@ -5,3 +5,23 @@ let guess;
 let hits = 0;
 let guesses = 0;
 let isSunk = false;
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number 0-6 :");
+    if (guess < 0 || guess > 6){
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses = guesses + 1;
+        if (guess == location1 || guess ==location2 || guess == location3){
+            alert("HIT!");
+            hits = hits +1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            } else {
+                alert("Miss!");
+            }
+        }
+    }
+}
+let stats = "You took" + guesses + "guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
